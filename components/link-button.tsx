e920018f1048d8e6397fd5662bad4b14e5f2dacf
Gaetan <gbusato@alteca.fr>
2021-01-26 09:37:49 +0100

    Changing index page and button to correct application bug

--- components/link-button.tsx
+++ components/link-button.tsx
@@ -10,28 +10,25 @@ type Props = {
 const setType = (type) => {
   switch (type) {
     case "light":
-      return "bg-white hover:text-black hover:bg-grey-200";
+      return "text-gray-900 bg-white hover:text-black hover:bg-grey-200  ";
     case "secondary":
-      return "bg-green-200 hover:bg-green-400 hover:text-white";
+      return "text-gray-900 bg-green-200 hover:bg-green-400 hover:text-white ";
     case "primary":
     default:
-      return "bg-pink-200 hover:bg-pink-400 hover:text-white";
+      return "text-gray-900 bg-pink-200 hover:bg-pink-400 hover:text-white ";
   }
 };
 
 const LinkButton = ({ text, type, link }: Props) => {
-  const cssColors = setType(type);
+  const cssColors =
+    "relative inline-block px-5 py-2 text-xl font-bold text-center no-underline whitespace-no-wrap align-middle border-2 border-gray-900 border-solid rounded-md shadow-xs cursor-pointer select-none hover:shadow-xs focus:shadow-xs " +
+    setType(type);
 
   return (
     <Link href={link}>
-      <a
-        className={
-          "relative z-10 text-lg mx-4 px-5 py-2 font-bold leading-tight text-black border-4 border-gray-900 rounded-lg  " +
-          cssColors
-        }
-      >
-        {text}
-        <span className="absolute h-full w-full bg-gray-900 rounded-lg inset-2 -z-1"></span>
+      <a className="relative inline-block m-2">
+        <div className="w-full h-full bg-black ml-0.5 mt-0.5 rounded-lg absolute left-0 top-0" />
+        <span className={cssColors}>{text}</span>
       </a>
     </Link>
   );
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,22 +1,37 @@
+import Image from "next/image";
 import React from "react";
 import LinkButton from "../components/link-button";
 import data from "../data/index.json";
 
 const Index = () => {
   return (
-    <div className="text-center mx-auto py-24">
-      <h1 className="text-7xl font-extrabold text-black leading-relaxed">
-        <div>{data.cta.title}</div>
-        <div className="text-pink-400">{data.cta.titleBright}</div>
-      </h1>
-      <p className="my-8 text-lg text-gray-700 max-w-2xl mx-auto">
-        {data.cta.subtitle}
-      </p>
-      <div className="my-16">
-        <LinkButton text={data.cta.btnSecondary} type="secondary" link="/" />
-        <LinkButton text={data.cta.btnPrimary} type="primary" link="/" />
+    <section>
+      <div className="flex px-5 py-24 items-center justify-center flex-col">
+        <Image
+          className="lg:w-2/6 md:w-3/6 mb-24 object-cover object-center"
+          src={data.cta.image}
+          width={200}
+          height={200}
+          alt={data.cta.imageAlt}
+          objectFit="cover"
+        />
+        <div className="text-center lg:w-2/3 w-full">
+          <h1 className="title-font text-5xl mb-4 font-extrabold leading-normal text-black my-12">
+            <div>{data.cta.title}</div>
+            <div className="text-pink-400">{data.cta.titleBright}</div>
+          </h1>
+          <p className="my-8 leading-relaxed text-xl">{data.cta.subtitle}</p>
+          <div className="flex justify-center flex-wrap-reverse">
+            <LinkButton
+              text={data.cta.btnSecondary}
+              type="secondary"
+              link="/"
+            />
+            <LinkButton text={data.cta.btnPrimary} type="primary" link="/" />
+          </div>
+        </div>
       </div>
-    </div>
+    </section>
   );
 };
 
